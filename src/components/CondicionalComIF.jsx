cf0cb2226bd8c84518d8ac47af1cd03dddca6626
Asafe Toschi <asafeluiztoschi@gmail.com>
2021-05-05 19:24:38 -0300

    Basic components and utilities

--- src/components/CondicionalComIF.jsx
+++ src/components/CondicionalComIF.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import If from'./If'
 const CondicionalComIF = (props) => {
     return (
--- src/components/Repeticao.jsx
+++ src/components/Repeticao.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import produtos from '../data/products';
 
 const Repepticao = () => {
